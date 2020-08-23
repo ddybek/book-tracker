9bfc4c8ad103b338a23850b7615b968280568bb6
ddybek <ddybek@gmail.com>
2020-08-23 20:40:49 +0200

    BT-5 Add basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { LoginComponent } from './login/login.component';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BooksComponent,
+    BookDetailsComponent,
+    AddBookComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'books/:isbn',
+        component: BookDetailsComponent
+      },
+      {
+        path: 'books',
+        component: BooksComponent
+      },
+      {
+        path: 'add-book',
+        component: AddBookComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
