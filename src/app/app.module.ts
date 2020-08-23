32e03cca2c7960d8d9c7354d6c4e445b929503a2
ddybek <ddybek@gmail.com>
2020-08-23 20:48:45 +0200

    BT-6 Add ng-bootstrap and basic navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { AddBookComponent } from './add-book/add-book.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { BooksComponent } from './books/books.component';
 import { HomeComponent } from './home/home.component';
@@ -17,7 +20,8 @@ import { environment } from '../environments/environment';
     BooksComponent,
     BookDetailsComponent,
     AddBookComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -43,7 +47,8 @@ import { environment } from '../environments/environment';
         path: 'add-book',
         component: AddBookComponent
       }
-    ])
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
